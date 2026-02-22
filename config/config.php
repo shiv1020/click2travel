41d860053efd4f1b6b0ebd53a3337c737c6690d3
shiv1020 <96096986+shiv1020@users.noreply.github.com>
2026-02-22 21:24:37 +0530

    Refactor BASE_URL definition for clarity

--- config/config.php
+++ config/config.php
@@ -1,5 +1,8 @@
 <?php
-define('BASE_URL', getenv('BASE_URL') ? rtrim(getenv('BASE_URL'), '/') . '/' : '/click2travel/public/');
+// define('BASE_URL', getenv('BASE_URL') ? rtrim(getenv('BASE_URL'), '/') . '/' : '/click2travel/public/');
+define('BASE_URL', getenv('BASE_URL') 
+    ? rtrim(getenv('BASE_URL'), '/') . '/' 
+    : '/');
 require_once __DIR__ . '/../app/core/Database.php';
 
 session_start();
