54792026dc1cf2a8eccf83eef859fc523a882ca2
AmanVI855 <amanvishawakarma246@gmail.com>
2026-02-22 20:27:27 +0530

    Fix deployment paths correctly

--- scripts/permissions.sh
+++ scripts/permissions.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-chmod -R 755 /var/www/click2travel
-<<<<<<< HEAD
-chown -R apache:apache /var/www/click2travel
-=======
-chown -R apache:apache /var/www/click2travel
->>>>>>> 944865d0e6bf86aecb46a51587121ff6eefe8497
+chmod -R 755 /var/www/html
+chown -R apache:apache /var/www/html
